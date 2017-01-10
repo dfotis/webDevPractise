741496a2db132b9abab19adbf8704586fdf5fe32
dfotis <fotis.dil94@gmail.com>
2017-01-10 14:15:36 +0200

    Add files via upload

---
+++ submit.php
@@ -0,0 +1,13 @@
+<?php
+include 'connect.php';
+date_default_timezone_set('Europe/Athens');
+			    if(isset($_POST['submit']))
+				{					
+					echo 'DONE';
+					$timestamp=date('Y-m-d H:i:s', time());
+					$email = mysql_real_escape_string($_POST['email']);
+					$comment = mysql_real_escape_string($_POST['comment']);
+					$insert = mysqli_query($link, "insert into comments (time_stamp,email,comment) values('$timestamp', '$email' ,'$comment')") or die(mysql_error());
+					header("Location:commentsPage.php");										
+				}
+?>
